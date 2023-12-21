47ae15357452568a80ce14a7c9489ec74219972e
youngjun.yoo <youngjuny@wemade.com>
2023-12-21 13:23:44 +0900

    nexus-repo commit

--- kubernetes/ssl-copy.sh
+++ kubernetes/ssl-copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 kubectl exec -n nexus3 pods/`kubectl -n nexus3 get pod -o jsonpath='{.items[0].metadata.name}'` -- keytool -printcert -sslserver nexus-service:8443 -rfc | tee nexus.crt
-security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ./nexus.crt
-mkdir -p ~/.docker/certs.d/nexus.wemade.com\:5443/
-cp -av ./nexus.crt   ~/.docker/certs.d/nexus.wemade.com\:5443/ca.crt
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ./nexus.crt
+mkdir -p ~/.docker/certs.d/nexus-service.nexus3.svc.cluster.local\:5443/
+cp -av ./nexus.crt   ~/.docker/certs.d/nexus-service.nexus3.svc.cluster.local\:5443/ca.crt
 rm ./nexus.crt
