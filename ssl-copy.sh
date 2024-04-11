d4f8103bfe30641983546cc9d8b5f2f7f82e4d11
youngjun.yoo <youngjuny@wemade.com>
2024-04-11 17:04:18 +0900

    .

--- ssl-copy.sh
+++ ssl-copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker exec pods/`kubectl -n nexus3 get pod -o jsonpath='{.items[0].metadata.name}'` keytool -printcert -sslserver nexus-service:8443 -rfc | tee nexus.crt
-security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ./nexus.crt
+docker-compose exec -it nexus keytool -printcert -sslserver localhost:8443 -rfc | tee nexus.crt
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ./nexus.crt
 mkdir -p ~/.docker/certs.d/nexus.wemade.com\:5443/
 cp -av ./nexus.crt   ~/.docker/certs.d/nexus.wemade.com\:5443/ca.crt
-  rm ./nexus.crt
+rm ./nexus.crt
